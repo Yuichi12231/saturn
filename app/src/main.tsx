e22f6061c916fd0e6d26aa84eacf6e65caa7edb4
Yuichi12231 <34160631+Yuichi12231@users.noreply.github.com>
2026-04-05 08:28:36 +0000

    Add browser Buffer polyfill and dependency for React app

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,3 +1,9 @@
+import { Buffer } from 'buffer';
+
+if (!globalThis.Buffer) {
+  globalThis.Buffer = Buffer;
+}
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
